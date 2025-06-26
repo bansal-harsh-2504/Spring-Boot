ffaf9846610ee4b6b80d4f4be744f168eb323671
harsh <focusedstudytarget2022@gmail.com>
2025-06-26 18:14:47 +0530

    added lombok

--- src/main/java/net/harsh/journalApp/entity/JournalEntryv2.java
+++ src/main/java/net/harsh/journalApp/entity/JournalEntryv2.java
@@ -1,5 +1,6 @@
 package net.harsh.journalApp.entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,44 +8,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntryv2 {
-
     @Id
     private ObjectId id;
     private String content;
     private String title;
-
     private LocalDateTime date;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
